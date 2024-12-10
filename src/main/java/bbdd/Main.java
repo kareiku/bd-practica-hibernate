d2f736f60b147c2644ea9bf0dc4afc574f43e1b3
kareiku <xaxvu7@gmail.com>
2024-12-10 17:16:43 +0100

    fixed hibernate.cfg.xml

--- src/main/java/bbdd/Main.java
+++ src/main/java/bbdd/Main.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -18,11 +19,8 @@ import bbdd.model.Pasajero;
 import bbdd.model.Entretenimiento;
 import bbdd.model.Gasto;
 
-
-public class Main 
-{
-    public static void main( String[] args )
-    {
+public class Main {
+    public static void main(String[] args) {
 
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure()
@@ -37,17 +35,15 @@ public class Main
         // TODO Crear un nuevo pasajero llamado "Din Djarin" y un nuevo entretenimiento
         //      llamado "Bounty Hunting" y guardarlos en la base de datos. Añade un gasto de
         //      100 a "Din Djarin" para "Bounty Hunting".
-        
+
 
         // TODO Leer el fichero CSV gastos.csv que se encuentra en el directorio "resources" y
         //      recorrerlo usando CSVParser para crear los pasajeros, entretenimientos y gastos que
         //      en él se encuentran. Dichos gastos deberán ser asignados al pasajero/a y al entretenimiento
         //      correspondientes. Se deben guardar todos estos datos en la base de datos.
-        
 
 
         session.close();
-
     }
 }
 

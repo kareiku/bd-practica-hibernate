21f9a9e498ed2661287d0d1791009bc1c17084e8
kareiku <xaxvu7@gmail.com>
2024-12-11 12:36:16 +0100

    minor changes

--- src/main/java/bbdd/Main.java
+++ src/main/java/bbdd/Main.java
@@ -23,33 +23,33 @@ public class Main {
         SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         Session session = sessionFactory.openSession();
 
-        Pasajero pasajero;
-        Entretenimiento entretenimiento;
-        Gasto gasto;
-
         /* ========================= Ej 4a ========================== */
-        pasajero = new Pasajero("Din Djarin");
-        entretenimiento = new Entretenimiento("Bounty Hunting");
-        gasto = new Gasto(pasajero, entretenimiento, 100);
-        session.beginTransaction();
-        session.saveOrUpdate(pasajero);
-        session.saveOrUpdate(entretenimiento);
-        session.saveOrUpdate(gasto);
-        session.getTransaction().commit();
+        {
+            Pasajero pasajero = new Pasajero("Din Djarin");
+            Entretenimiento entretenimiento = new Entretenimiento("Bounty Hunting");
+            Gasto gasto = new Gasto(pasajero, entretenimiento, 100);
+            session.beginTransaction();
+            session.saveOrUpdate(pasajero);
+            session.saveOrUpdate(entretenimiento);
+            session.saveOrUpdate(gasto);
+            session.getTransaction().commit();
+        }
 
         /* ========================= Ej 4b ========================== */
-        try (CSVParser parser = new CSVParser(new FileReader("/home/kare/IdeaProjects/bd-practica-hibernate/src/main/resources/gastos.csv"), CSVFormat.DEFAULT.withHeader())) {
-            session.beginTransaction();
-            for (CSVRecord record : parser) {
-                pasajero = new Pasajero(record.get(0));
-                entretenimiento = new Entretenimiento(record.get(1));
-                gasto = new Gasto(pasajero, entretenimiento, Integer.parseInt(record.get(2)));
-                session.saveOrUpdate(pasajero);
-                session.saveOrUpdate(entretenimiento);
-                session.saveOrUpdate(gasto);
+        {
+            try (CSVParser parser = new CSVParser(new FileReader("/home/kare/IdeaProjects/bd-practica-hibernate/src/main/resources/gastos.csv"), CSVFormat.DEFAULT.withHeader())) {
+                session.beginTransaction();
+                for (CSVRecord record : parser) {
+                    Pasajero pasajero = new Pasajero(record.get(0));
+                    Entretenimiento entretenimiento = new Entretenimiento(record.get(1));
+                    Gasto gasto = new Gasto(pasajero, entretenimiento, Integer.parseInt(record.get(2)));
+                    session.saveOrUpdate(pasajero);
+                    session.saveOrUpdate(entretenimiento);
+                    session.saveOrUpdate(gasto);
+                }
+                session.getTransaction().commit();
+            } catch (IOException ignore) {
             }
-            session.getTransaction().commit();
-        } catch (IOException ignore) {
         }
 
         session.close();

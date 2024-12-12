350182db768c450c675589b2549b29b0c5c2bea5
kareiku <xaxvu7@gmail.com>
2024-12-12 18:37:31 +0100

    added selects for all data of each separate table, too

--- src/main/resources/consultas_hibernate.sql
+++ src/main/resources/consultas_hibernate.sql
@@ -2,6 +2,10 @@ DROP SCHEMA IF EXISTS `titanic_spaceship`;
 
 CREATE SCHEMA `titanic_spaceship`;
 
+SELECT * FROM `titanic_spaceship`.`pasajeros`;
+SELECT * FROM `titanic_spaceship`.`entretenimientos`;
+SELECT * FROM `titanic_spaceship`.`gastos`;
+
 SELECT *
 FROM `titanic_spaceship`.`gastos`
 JOIN `titanic_spaceship`.`entretenimientos` ON `entretenimientos`.`id`=`gastos`.`id_entretenimiento`

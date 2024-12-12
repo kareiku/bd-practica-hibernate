719b7e374928d7c1882e784d848ce6d5b79563b5
kareiku <xaxvu7@gmail.com>
2024-12-12 18:33:39 +0100

    lower-cased the word "hunting" in "Bounty Hunting"

--- src/main/java/bbdd/utils/options/Exercise4aOption.java
+++ src/main/java/bbdd/utils/options/Exercise4aOption.java
@@ -8,7 +8,7 @@ import org.hibernate.Session;
 public class Exercise4aOption extends Option {
     public void execute(Session session) {
         Pasajero pasajero = new Pasajero("Din Djarin");
-        Entretenimiento entretenimiento = new Entretenimiento("Bounty Hunting");
+        Entretenimiento entretenimiento = new Entretenimiento("Bounty hunting");
         Gasto gasto = new Gasto(pasajero, entretenimiento, 100);
         session.save(pasajero);
         session.save(entretenimiento);

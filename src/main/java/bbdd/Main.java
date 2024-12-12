900c00f815f16822f1f5d9a7925a68e0ccf532c3
kareiku <xaxvu7@gmail.com>
2024-12-12 17:52:34 +0100

    delegated exception management and changed session parameter by factory one

--- src/main/java/bbdd/Main.java
+++ src/main/java/bbdd/Main.java
@@ -2,7 +2,6 @@ package bbdd;
 
 import bbdd.utils.Menu;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -12,8 +11,6 @@ public class Main {
     public static void main(String[] args) {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        Session session = factory.openSession();
-        new Menu(session).start();
-        session.close();
+        new Menu(factory).start();
     }
 }

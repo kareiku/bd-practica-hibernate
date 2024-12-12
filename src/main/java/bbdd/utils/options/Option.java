14eb01fa75906c5e459fa78fe23aaf8314094d48
kareiku <xaxvu7@gmail.com>
2024-12-12 18:04:04 +0100

    corrected by using try-with-resources to avoid using invalidated sessions and transactions

--- src/main/java/bbdd/utils/options/Option.java
+++ src/main/java/bbdd/utils/options/Option.java
@@ -1,7 +1,18 @@
 package bbdd.utils.options;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
-public interface Option {
-    void execute(Session session);
+public abstract class Option {
+    public void tryExecution(SessionFactory factory) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            this.execute(session);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            assert false : ex.getMessage();
+        }
+    }
+
+    protected abstract void execute(Session session);
 }
